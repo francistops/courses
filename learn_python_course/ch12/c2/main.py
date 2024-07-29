f5c33768de2fca12474f5c2ae0585eb44e4fada5
francistops <65456051+francistops@users.noreply.github.com>
2024-07-28 20:30:07 -0400

    complete ch 12 with challenges on error handling

---
+++ learn_python_course/ch12/c2/main.py
@@ -0,0 +1,42 @@
+def make_purchases(purchase_orders):
+    leftover = []
+    for po in purchase_orders:
+        try:
+            money_leftover = purchase(po['price'],
+                                      po['money_available'])
+        except Exception as err:
+            print(err)
+        if money_leftover > 0:
+            leftover.append(money_leftover)
+    return leftover
+
+    # Don't edit below this line
+
+
+def main():
+    print("Making purchases...")
+    leftovers = make_purchases(
+        [
+            {"price": 10.00, "money_available": 125.00},
+            {"price": 5.00, "money_available": 2.00},
+            {"price": 20.01, "money_available": 5.20},
+            {"price": 1.04, "money_available": 254.00},
+            {"price": 40.20, "money_available": 6.00},
+            {"price": 16.00, "money_available": 235.01},
+            {"price": 10.70, "money_available": 10.70},
+            {"price": 12.00, "money_available": 2.30},
+        ]
+    )
+    print("Purchases complete!")
+    print("Leftover amounts for successful purchases:")
+    for leftover in leftovers:
+        print(f" * {leftover:.2f}")
+
+
+def purchase(price, money_available):
+    if money_available < price:
+        raise Exception(f"{money_available:.2f} is not enough for {price:.2f}")
+    return money_available - price
+
+
+main()
