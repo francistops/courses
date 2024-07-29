9c0995583216e957ad4bbf6f74701d526b1148b8
francistops <65456051+francistops@users.noreply.github.com>
2024-07-29 19:26:22 -0400

    done with the project bookbot on boot.dev

--- build_a_bookbot/bookbot/main.py
+++ build_a_bookbot/bookbot/main.py
@@ -1,3 +1,4 @@
+# my code for the project (same output as instructor)
 import string
 
 
@@ -5,12 +6,20 @@ def main():
     book_path = 'books/frankenstein.txt'
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+
+    print(f'--- Begin report of {book_path} ---')
     print(f'{num_words} words found in the document')
+    print()
 
     letters_count = {}
     for letter in string.ascii_lowercase:
         letters_count[letter] = get_num_letter(text, letter)
-    print(letters_count)
+
+    for letter in sorted(letters_count, key=letters_count.get, reverse=True):
+        print(f"The '{letter}' character was found {
+              letters_count[letter]} times")
+
+    print('--- End report ---')
 
 
 def get_num_words(text):
